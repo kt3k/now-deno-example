abb260bb303ec5e3c038e40e5daa27b6ade50752
Yoshiya Hinosawa <stibium121@gmail.com>
2020-02-14 01:10:02 +0900

    feat: api/read.ts

--- api/read.ts
+++ api/read.ts
@@ -1,17 +1,27 @@
-// api/hello.ts
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   Context
 } from "https://deno.land/x/lambda/mod.ts";
+import { Client } from "https://deno.land/x/postgres/mod.ts";
+
+async function connect() {
+  const client = new Client();
+  await client.connect();
+  return client
+}
+
+const connection = connect()
 
 export async function handler(
   event: APIGatewayProxyEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> {
+  const client = await connection
+  const result = await client.query("SELECT * FROM posts LIMIT 16;")
   return {
     statusCode: 200,
-    body: JSON.stringify(Deno.version),
+    body: JSON.stringify(result.rows),
     headers: {
       "content-type": "application/json; charset=utf-8"
     }
