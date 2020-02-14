592fb3913c12e3059af5b68a5dde64df98388793
Yoshiya Hinosawa <stibium121@gmail.com>
2020-02-15 02:15:27 +0900

    refactor: fmt

--- api/post.ts
+++ api/post.ts
@@ -9,32 +9,32 @@ import { Client } from "https://deno.land/x/postgres/mod.ts";
 async function connect() {
   const client = new Client();
   await client.connect();
-  return client
+  return client;
 }
 
-const connection = connect()
+const connection = connect();
 
 export async function handler(
   event: APIGatewayProxyEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> {
-  const body = JSON.parse(event.body)
+  const body = JSON.parse(event.body);
   const json = new TextDecoder().decode(base64.toUint8Array(body.body));
-  const req = JSON.parse(json)
+  const req = JSON.parse(json);
   if (!req || !req.user || !req.body) {
     return {
       statusCode: 400,
-      body: JSON.stringify({ message: 'bad request' }),
+      body: JSON.stringify({ message: "bad request" }),
       headers: {
-        'content-type': 'application/json'
+        "content-type": "application/json"
       }
-    }
+    };
   }
-  const client = await connection
+  const client = await connection;
   const result = await client.query(
     "INSERT INTO posts (name, body) values ($1, $2);",
     req.user,
     req.body
-  )
-  return { statusCode: 200, body: '' };
+  );
+  return { statusCode: 200, body: "" };
 }
